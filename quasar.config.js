431e0506c1d1dfb2363523666609b7aefc8b907b
米图斯 <1034896732@qq.com>
2025-05-03 00:06:11 +0800

    少提交了一个文件

--- quasar.config.js
+++ quasar.config.js
@@ -12,7 +12,7 @@ export default defineConfig((ctx) => {
     // app boot file (/src/boot)
     // --> boot files are part of "main.js"
     // https://v2.quasar.dev/quasar-cli-vite/boot-files
-    boot: ['i18n', 'axios', 'head'],
+    boot: ['i18n', 'axios', 'head', 'google-services'],
 
     // https://v2.quasar.dev/quasar-cli-vite/quasar-config-file#css
     css: ['app.scss'],
